a610e8ef22488720da27a39de8856ba8433caa02
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-11-08 14:59:48 +0100

    Cosmetic adjustments

--- src/model/hotspot.rs
+++ src/model/hotspot.rs
@@ -2,10 +2,8 @@
 
 extern crate hotspot;
 
-use super::super::Circuit;
-
-/// Constructs a thermal RC circuit using the HotSpot model.
-pub fn new(floorplan: &Path, config: &Path, params: &str) -> Result<Circuit, &'static str> {
+/// Construct a thermal RC circuit using the HotSpot model.
+pub fn new(floorplan: &Path, config: &Path, params: &str) -> Result<::Circuit, &'static str> {
     use std::mem::transmute;
     let circuit = try!(hotspot::Circuit::new(floorplan, config, params.as_slice()));
     Ok(unsafe { transmute(circuit) })
