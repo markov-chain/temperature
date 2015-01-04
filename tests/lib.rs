057e2815b4604c086a6a03d870710e18234c493b
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-01-04 06:28:35 +0100

    Updated to Rust’s master

--- tests/lib.rs
+++ tests/lib.rs
@@ -8,14 +8,16 @@ mod support;
 #[test]
 #[allow(non_snake_case)]
 fn compute_transient() {
+    use std::iter::repeat;
+
     let analysis = support::setup("002");
 
     let nc = 2;
     let nn = 4 * nc + 12;
     let ns = fixture::P.len() / nc;
 
-    let mut Q = Vec::from_elem(nc * ns, 0.0);
-    let mut S = Vec::from_elem(nn * ns, 0.0);
+    let mut Q = repeat(0.0).take(nc * ns).collect::<Vec<_>>();
+    let mut S = repeat(0.0).take(nn * ns).collect::<Vec<_>>();
 
     analysis.compute_transient(fixture::P.as_slice(), Q.as_mut_slice(),
                                S.as_mut_slice(), ns);
