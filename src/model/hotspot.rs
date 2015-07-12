d14c39910ce4fccc3ae8dd6d2872d8d7a8c3001b
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-12 13:10:51 -0400

    Do not transmute

--- src/model/hotspot.rs
+++ src/model/hotspot.rs
@@ -5,9 +5,17 @@ extern crate hotspot;
 use std::io::Result;
 use std::path::Path;
 
+use Circuit;
+
 /// Construct a thermal RC circuit using the HotSpot model.
-pub fn new(floorplan: &Path, config: &Path, params: &str) -> Result<::Circuit> {
-    use std::mem::transmute;
-    let circuit = try!(hotspot::Circuit::new(floorplan, config, &params));
-    Ok(unsafe { transmute(circuit) })
+pub fn new(floorplan: &Path, config: &Path, params: &str) -> Result<Circuit> {
+    let hotspot::Circuit { cores, nodes, capacitance, conductance } = {
+        try!(hotspot::Circuit::new(floorplan, config, &params))
+    };
+    Ok(Circuit {
+        cores: cores,
+        nodes: nodes,
+        capacitance: capacitance,
+        conductance: conductance,
+    })
 }
