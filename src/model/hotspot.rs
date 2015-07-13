8a92a62b96b7a0e879cf26bef76f775332570f9e
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-13 17:18:38 -0400

    Update hotspot

--- src/model/hotspot.rs
+++ src/model/hotspot.rs
@@ -1,6 +1,5 @@
 extern crate hotspot;
 
-use matrix::{Compressed, Dense, Diagonal, Make, Shape};
 use std::path::Path;
 
 use {Circuit, Result};
@@ -11,14 +10,12 @@ pub struct HotSpot;
 impl HotSpot {
     /// Construct a thermal circuit.
     pub fn new<F: AsRef<Path>, C: AsRef<Path>>(floorplan: F, config: C) -> Result<Circuit> {
-        let hotspot::Circuit { nodes, cores, capacitance, conductance }  = {
-            ok!(hotspot::Circuit::new(floorplan, config))
-        };
+        let circuit = ok!(hotspot::Circuit::new(floorplan, config));
         Ok(Circuit {
-            cores: cores,
-            nodes: nodes,
-            capacitance: Diagonal::make(capacitance, Shape::Square(nodes)),
-            conductance: Compressed::from(Dense::make(conductance, Shape::Square(nodes))),
+            cores: circuit.cores,
+            nodes: circuit.nodes,
+            capacitance: circuit.capacitance,
+            conductance: circuit.conductance,
         })
     }
 }
