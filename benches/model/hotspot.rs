5f50c39eecc91599c9f5944d29e050a940866a9d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-14 08:24:21 -0400

    Update the benchmarks

--- benches/model/hotspot.rs
+++ benches/model/hotspot.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use random::{self, Source};
 use std::default::Default;
 use std::fs;
@@ -7,23 +9,22 @@ use test::Bencher;
 use temperature::Analysis;
 use temperature::model::HotSpot;
 
-#[bench]
-#[allow(non_snake_case)]
-fn compute_transient(bench: &mut Bencher) {
-    let analysis = setup("032");
+#[bench] fn step_0001(bench: &mut Bencher) { step(   1, bench); }
+#[bench] fn step_0010(bench: &mut Bencher) { step(  10, bench); }
+#[bench] fn step_0100(bench: &mut Bencher) { step( 100, bench); }
+#[bench] fn step_1000(bench: &mut Bencher) { step(1000, bench); }
 
-    let nc = 32;
-    let ns = 1000;
-    let nn = 4 * nc + 12;
+fn step(steps: usize, bench: &mut Bencher) {
+    let cores = 32;
 
-    let P = random::default().iter().take(nc * ns).collect::<Vec<_>>();
-    let mut Q = vec![0.0; nc * ns];
-    let mut S = vec![0.0; nn * ns];
+    let mut analysis = setup("032");
+    let P = random::default().iter().take(steps * cores).collect::<Vec<_>>();
+    let mut Q = vec![0.0; steps * cores];
 
-    bench.iter(|| analysis.compute_transient(&P, &mut Q, &mut S, ns));
+    bench.iter(|| analysis.step(&P, &mut Q));
 }
 
-pub fn setup(name: &str) -> Analysis {
+fn setup(name: &str) -> Analysis {
     let circuit = HotSpot::new(find(&format!("{}.flp", name)), find("hotspot.config"));
     Analysis::new(&circuit.unwrap(), &Default::default()).unwrap()
 }
