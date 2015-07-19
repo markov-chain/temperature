0a60a52524daf4d00a1d39e4488e523d105e2c9e
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-19 09:23:10 -0400

    Add ThreeDICE::from

--- src/circuit/threed_ice.rs
+++ src/circuit/threed_ice.rs
@@ -1,6 +1,7 @@
 extern crate threed_ice;
 
 use matrix::operation::Transpose;
+use self::threed_ice::System;
 use std::path::Path;
 
 use {Circuit, Result};
@@ -9,14 +10,18 @@ use {Circuit, Result};
 pub struct ThreeDICE;
 
 impl ThreeDICE {
-    /// Construct a thermal circuit.
+    /// Construct a thermal circuit given a configuration file.
     pub fn new<T: AsRef<Path>>(config: T) -> Result<Circuit> {
-        let backend = ok!(threed_ice::System::new(config));
-        let distribution = ok!(backend.distribution());
+        ThreeDICE::from(&ok!(System::new(config)))
+    }
+
+    /// Construct a thermal circuit given a system.
+    pub fn from(system: &System) -> Result<Circuit> {
+        let distribution = ok!(system.distribution());
         let aggregation = distribution.transpose();
         Ok(Circuit {
-            capacitance: ok!(backend.capacitance()),
-            conductance: ok!(backend.conductance()),
+            capacitance: ok!(system.capacitance()),
+            conductance: ok!(system.conductance()),
             distribution: distribution,
             aggregation: aggregation,
         })
