54c0593306b553d43031088ab593131ffa233928
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-06 17:09:25 -0400

    3D-ICE: add a test for the conductance matrix

--- tests/threed_ice/mod.rs
+++ tests/threed_ice/mod.rs
@@ -4,7 +4,7 @@ use assert;
 use matrix::format::{Conventional, Diagonal};
 use std::path::PathBuf;
 use temperature::circuit::ThreeDICE;
-use temperature::{Config, Simulator};
+use temperature::{Circuit, Config, Simulator};
 
 mod fixture;
 
@@ -19,9 +19,44 @@ fn ambience() {
 }
 
 #[test]
+fn conductance() {
+    use matrix::{Size, Matrix};
+    use matrix::decomposition::SymmetricEigen;
+    use matrix::operation::{MultiplyInto, MultiplySelf, Transpose};
+
+    let Circuit { conductance, distribution, .. } = ThreeDICE::new(find("004.stk")).unwrap();
+    let (nodes, units) = distribution.dimensions();
+
+    let mut A = Conventional::zero(nodes);
+    {
+        let mut U = Conventional::from(conductance);
+        let mut L = Diagonal::zero(nodes);
+        SymmetricEigen::decompose(&mut (&mut *U, &mut *L)).unwrap();
+        for lambda in L.iter_mut() {
+            *lambda = 1.0 / *lambda;
+        }
+        U.multiply_self(&L);
+        U.multiply_into(&U.transpose(), &mut A);
+    }
+
+    let mut P = vec![];
+    for i in 0..units {
+        P.push(10.0 * (i as f64 + 1.0));
+    }
+    P.extend(vec![0.0; nodes - units]);
+
+    let mut Q = vec![318.15; nodes];
+    A.multiply_into(&P, &mut Q);
+    Q.truncate(units);
+
+    assert::close(&Q, &vec![3.556272578548002e+02, 3.856526078345531e+02,
+                            4.156779578143060e+02, 4.457033077940591e+02], 1e-14);
+}
+
+#[test]
 fn distribution() {
-    let circuit = ThreeDICE::new(find("004.stk")).unwrap();
-    let distribution = Conventional::from(&circuit.distribution);
+    let Circuit { distribution, .. } = ThreeDICE::new(find("004.stk")).unwrap();
+    let distribution = Conventional::from(&distribution);
     let identity = Conventional::from(Diagonal::from_vec((4 * UNITS, UNITS), vec![1.0; UNITS]));
     assert::close(&*distribution, &*identity, 1e-15);
 }
